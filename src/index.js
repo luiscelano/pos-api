20ec20bf9986caa7df2709ecd51c399cece65bc1
luiscelano <luiscelano123@live.com>
2022-10-25 23:37:07 -0600

    fix: added static directory for views

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import http from 'http'
+import path from 'path'
 import { initAPI } from 'api'
 
 const app = express()
@@ -10,13 +11,14 @@ const PORT = 3000
 const server = http.createServer(app)
 
 initAPI(app)
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.get('/healthcheck', (req, res) => {
   res.send('Ok')
 })
 
 app.get('/homepage', (req, res) => {
-  res.sendFile(__dirname + '/views/home.html')
+  res.sendFile(path.resolve(__dirname + '/views/home.html'))
 })
 
 server.listen(PORT, HOST, () => {
